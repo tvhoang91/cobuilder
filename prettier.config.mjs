2df509b8f058994ec4ca31c013a6c9beda483916
Hoang Truong <tvhoang91@gmail.com>
2025-08-13 11:28:50 +0700

    formatter

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+export default {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-tailwindcss'],
+}
