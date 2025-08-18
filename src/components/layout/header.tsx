ea91bea76f2bab5a73e0c86f2eaf886d53ef66fe
Hoang Truong <tvhoang91@gmail.com>
2025-08-18 14:35:39 +0700

    fix header center

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,7 +3,7 @@
 export default async function Header({ children }: { children: React.ReactNode }) {
   return (
     <header className="border-border bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
-      <div className="flex h-14 max-w-screen-2xl items-center px-6">{children}</div>
+      <div className="mx-auto flex h-14 max-w-screen-2xl items-center px-6">{children}</div>
     </header>
   )
 }
